04234d7a5b4f69b373ebd10d8c8ef1e4a03227f4
Rut_Kramer <Rut@leader.codes>
2020-12-07 16:19:51 +0200

    i commit

--- src/components/Store/Store.js
+++
@@ -1,8 +0,0 @@
-import {createStore} from 'redux'
-import reducer from '../reducer';
-const initialState={
-    list:[],
-    userName:'ytu'
-}
-const store=createStore(reducer,initialState)
-export default store;
