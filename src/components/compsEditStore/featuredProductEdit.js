7f5466d7b51e8312eadc692b4429f0c3f932966d
sariHilman <sarih@leader.codes>
2020-12-20 13:10:01 +0200

    change sari

--- src/components/compsEditStore/featuredProductEdit.js
+++ src/components/compsEditStore/featuredProductEdit.js
@@ -2,19 +2,16 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+
+
+
 import Switch from '@material-ui/core/Switch';
-// import SettingsIcon from '@material-ui/icons/Settings';
 import SettingsIcon from '@material-ui/icons/Settings';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import { withStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
-
-//הוספתי את זה
-// import  IconButton  from '@material-ui/core';
-//
 import Typography from '@material-ui/core/Typography';
-
 import FormLabel from '@material-ui/core/FormLabel';
 import Box from '@material-ui/core/Box';
 import FormControl from '@material-ui/core/FormControl';
@@ -271,8 +268,7 @@ const useStyles = (theme) => ({
         marginTop: '10px'
     }
 });
-
-class TextTitleOfProduct extends Component {
+class FeaturedProductEdit extends Component {
     constructor(props) {
         super(props)
         this.state = {
@@ -349,7 +345,7 @@ class TextTitleOfProduct extends Component {
                                 </div>
                                 :
                                 <div className={classes.row} >
-                                    <h3 edge="end" className={classes.titleSettings}>  Title Setting of product </h3>
+                                    <h3 edge="end" className={classes.titleSettings}>  Title Setting of feature Product </h3>
                                     <div edge="start">
                                         <label for="ColapseLogo">
                                             <KeyboardArrowDownIcon />
@@ -506,6 +502,7 @@ class TextTitleOfProduct extends Component {
                                         display="flex"
                                         justifyContent="space-between"
                                     >
+
                                         <Box
                                             width={'100%'}
                                             alignSelf="center"
@@ -607,4 +604,4 @@ const mapDispatchToProps = (dispatch) => ({
 })
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(useStyles)(TextTitleOfProduct));
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(useStyles)(FeaturedProductEdit));
