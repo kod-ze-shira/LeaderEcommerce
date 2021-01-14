4725d177ff2e86fc07719daa6585a64e5948a260
Rut Kramer <rut@leader.codes>
2021-01-14 14:43:53 +0200

    server return the new store

--- src/redux/middleWares/crud.js
+++ src/redux/middleWares/crud.js
@@ -423,15 +423,14 @@ export const createNewStore = ({ dispatch, getState }) => next => action => {
     //שם הפונקציה בקומפוננטה צריכה להיות כמו השם הזה רק עם אותיות גדולות מפרידות בין מילה למילה
     if (action.type === 'CREATE_NEW_STORE') {
         debugger;
-        // var storeManager = getState().userReducer.user._id;
-        var myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
+        // var myHeaders = new Headers();
+        // myHeaders.append("Content-Type", "application/json");
         console.log("user from redux", getState().userReducer.user);
         //בקומפוננטה צריך לשלוח לפונ' את האוביקט שעוטף את כל שדות החנות
         var raw = JSON.stringify({
             "storeName": action.payload.nameStore,
             "storeDescription": action.payload.descriptionStore,
-            "logo": action.payload.logoStore,
+            // "logo": action.payload.logoStore,
             "address": action.payload.addressStore,
             "tel": action.payload.phoneStore,
             "email": action.payload.emailStore,
@@ -440,18 +439,41 @@ export const createNewStore = ({ dispatch, getState }) => next => action => {
             "currency": action.payload.currency,
             "policy": action.payload.policy
         });
-        var requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: raw,
-            redirect: 'follow'
-        };
-        fetch("https://community.leader.codes/api/stores/newStore", requestOptions)
-            .then(response => { console.log(response); response.json() })
-            //.then(result => {console.log(result); dispatch(actions.setStore(result))})
-            .catch(error => console.log('error', error));
-    }
+        // var requestOptions = {
+        //     method: 'POST',
+        //     headers: myHeaders,
+        //     body: raw,
+        //     redirect: 'follow'
+        // };
+        console.log("raww",raw)
+    //     fetch("https://community.leader.codes/api/stores/newStore", requestOptions)
+    //         .then(store => { let o=store.json() ; console.log(o);})
+    //         //.then(result => {console.log(result); dispatch(actions.setStore(result))})
+    //         .catch(error => console.log('error', error));
+    // }
 
+
+
+
+    $.ajax({
+        url: "https://community.leader.codes/api/stores/newStore",
+        method: "post",
+        dataType: "json",
+        contentType: "application/json",
+        data: raw,
+        success: function (data) {
+            console.log(data)
+
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            console.log(XMLHttpRequest, " ", textStatus, " ", errorThrown)
+
+        }
+    });
+
+
+
+    }
     return next(action);
 };
 
